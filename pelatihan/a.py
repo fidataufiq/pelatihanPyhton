bd9bed3b477c25109f72171a6153ee10020b017f
Fida Taufiq <89764956+fidataufiq@users.noreply.github.com>
2022-09-10 10:27:50 +0700

    Add files via upload

---
+++ pelatihan/a.py
@@ -0,0 +1,52 @@
+#belajar elemen dasar python
+print("selamat Pagi")
+
+bendaBerat = "laptop"
+print("saya mempunya benda bernama "+bendaBerat)
+
+#tipe data di python: 1.string 2.int 3.float 4.Boolean
+
+#string
+string = "9"
+print (string)
+
+#int
+int = 10
+print ("ini integer",+int)
+
+#float
+float = 10,5
+
+
+#boolean
+#false and true
+print ('tipe data boolean',+ 10 > 9)
+
+a = 45
+b = 15
+print ('termasuk tipe data boolean apa?',+a == b)
+
+#operator aritmatika
+# (+),(-),(*),(/),(**),(%)
+
+#(+) penjumlahan
+a=10
+b=5
+print ('hasil dari penjumlahan ini adalah =',+ a+b)
+
+#pengurangan
+print ('hasil dari pengurangan ini adalah =',+ a-b)
+
+#perkalian
+print ('hasil dari perkalian ini adalah =',+ a*b)
+
+#pembagian
+print ('hasil dari pembagian ini adalah =',+ a/b)
+
+#kuadrat
+print ('hasil dari kuadrat ini adalah =',+ a**b)
+
+#modulus
+a=7
+b=2
+print ('hasil dari modulus ini adalah =',+ a%b)
